750a11a75eae00030eacc3fb5338b388e8140b7e
Md Habibur Rahman <shakibdshy@gmail.com>
2022-07-21 21:30:26 -0700

    Navigation Top Section Update

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,8 +1,29 @@
+import Image from 'next/image'
 import React from 'react'
+import author from '../assets/author2.jpg'
+import { FiSearch } from 'react-icons/fi'
 
 function HomePage() {
   return (
-    <div>Home</div>
+    <>
+      <section className='p-6 pl-10 border-b border-[#272a38]'>
+        <div className=''>
+          <ul className='flex items-center gap-5'>
+            <li className='text-white font-medium text-2xl'>Home</li>
+            <li className='ml-auto grow-0 shrink-0 basis-auto'>
+              <form className='relative z-10'>
+                <input type="text" name="name" placeholder='Search by keyword' className='search-input' />
+                <FiSearch className='search-icon' />
+              </form>
+            </li>
+            <li className='w-14 h-14'>
+              <Image src={author} alt="Author Image" width={100} height={100} objectFit='cover' quality={100} className='rounded-xl' />
+            </li>
+          </ul>
+
+        </div>
+      </section>
+    </>
   )
 }
 
