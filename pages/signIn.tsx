4263e4e230bac3c56b6e17d4a6e2e00e046f43fc
Md Habibur Rahman <shakibdshy@gmail.com>
2022-07-29 20:37:01 +0600

    Update Sign in page

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -6,58 +6,48 @@ import { FcGoogle } from "react-icons/fc";
 
 function signIn() {
   return (
-    <div className="section max-w-2xl bg-white h-auto mx-auto">
+    <div className="p-12 pb-0 section max-w-2xl bg-[#232634] h-auto mt-20 mx-auto shadow-lg rounded-2xl">
       <div className="title">
-        <h1 className="text-4xl text-[#83bbff] font-bold text-center my-8 flex justify-center">
+        <h1 className="text-4xl text-[#83bbff] font-bold text-center pt-14 pb-8 flex justify-center">
           Zoomla
         </h1>
       </div>
       <form className="grid justify-items-center gap-y-4">
-        <input
-          className="border border-[#83bbff] px-24 py-2 ruonded-lg"
-          type="text"
-          name="email"
-          placeholder="Enter Your Email"
-        />
-        <input
-          className="border border-stone-400 px-24 py-2 rounded-lg"
-          type="text"
-          name="password"
-          placeholder="Enter Your Password"
-        />
-        <Button className="px-36 bg-slate-300" variant="outlined">
+        <input type="text" name="name" placeholder='Enter Your Email' className='search-input shadow-md py-4 pl-8' />
+        <input type="text" name="name" placeholder='Enter Your Password' className='search-input shadow-md py-4 pl-8' />
+        <Button className="px-36 bg-slate-300">
           Sign In
         </Button>
       </form>
       <div className="checkbox flex justify-center items-center font-normal mt-5">
         <Checkbox defaultChecked />
-        <h2>Keep me signed in</h2>
+        <h2 className='text-white'>Keep me signed in</h2>
       </div>
       <div className="flex justify-center items-center gap-3">
         <hr className="w-full h-[1px] bg-grey-300 border-none" />
-        <h2 className="grow-0 shrink-0 basis-auto m-5">or Sign In with</h2>
+        <h2 className="grow-0 shrink-0 basis-auto m-5 text-white">or Sign In with</h2>
         <hr className="w-full h-[1px] bg-grey-300 border-none" />
       </div>
-      <div className="grid justify-items-center">
-        <ul className="flex gap-6">
-          <li className="">
-            <FcGoogle/>
+      <div className="grid justify-items-center mt-8">
+        <ul className="flex gap-8">
+          <li className="flex flex-col items-center justify-center text-white">
+            <FcGoogle className="text-xl" />
             Google</li>
-          <li>
-            <BsFacebook />
+          <li className="flex flex-col items-center justify-center text-white">
+            <BsFacebook className="text-xl" />
             Facebook</li>
-          <li>
-            <BsGithub />
+          <li className="flex flex-col items-center justify-center text-white">
+            <BsGithub className="text-xl" />
             GitHub</li>
         </ul>
       </div>
       <div className="flex justify-between">
-        <p className="flex justify-center items-center ml-10 m-10 gap-x-2">
-          <Link href='/'>
-          <a><BsChevronLeft/></a>
-          </Link>
-          Back</p>
-        <p className="text-[#83bbff] mr-10 m-10">Sign Up</p>
+        <Link href='/'>
+          <a className="flex justify-center items-center ml-10 m-10 gap-x-2 text-white"><BsChevronLeft /> Back</a>
+        </Link>
+        <Link href='/'>
+          <a className="text-[#83bbff] mr-10 m-10">Sign Up</a>
+        </Link>
       </div>
     </div>
   );
