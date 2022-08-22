4504daf8470bee77b4c48a0408de024d2aab21ec
Md Habibur Rahman <shakibdshy@gmail.com>
2022-08-22 18:26:13 +0600

    Update ChannelListContainer

--- src/components/ChatContainer/ChannelListContainer.jsx
+++ src/components/ChatContainer/ChannelListContainer.jsx
@@ -1,9 +1,32 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 import React from 'react'
+import ZoomlaIcon from '../../assets/zoompost.jpg'
+import LogoutIcon from '../../assets/logout.png'
+
+
+export const SideBar = () => {
+  return (
+    <div className="channel-list__sidebar">
+      <div className="channel-list__sidebar__icon1">
+        <div className="icon1__inner">
+          <img src={ZoomlaIcon} alt="Zoomla Icon" width="30" />
+        </div>
+      </div>
+      <div className="channel-list__sidebar__icon2">
+        <div className="icon1__inner">
+          <img src={LogoutIcon} alt="Logout Icon" width="30" />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const ChannelListContainer = () => {
   return (
-    <div>ChannelListContainer</div>
+    <>
+      <SideBar />
+    </>
   )
 }
 
