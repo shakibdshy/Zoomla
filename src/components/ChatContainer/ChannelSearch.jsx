c8dd7120ab7e65e1d1ceff251a29e10e551dfda2
Md Habibur Rahman <shakibdshy@gmail.com>
2022-08-25 21:00:16 +0600

    Update Channel Search Filter

--- src/components/ChatContainer/ChannelSearch.jsx
+++ src/components/ChatContainer/ChannelSearch.jsx
@@ -1,43 +1,86 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable prettier/prettier */
-import { SearchIcon } from '@100mslive/react-icons'
-import React, { useState } from 'react'
+import { SearchIcon } from "@100mslive/react-icons";
+import React, { useState, useEffect } from "react";
+import { useChatContext } from "stream-chat-react";
 
-const ChannelSearch = () => {
-    const [query, setQuery] = useState('')
-    const [loading, setLoading] = useState(false)
+import { ResultsDropdown } from "./";
 
-    const getChannels = async (text) => { 
-        try { 
-            // Todo: fetch channels
-        } catch (error) { 
-            setQuery('')
-        }
+const ChannelSearch = ({ setToggleContainer }) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [teamChannels, setTeamChannels] = useState([]);
+  const [directChannels, setDirectChannels] = useState([]);
+
+  useEffect(() => {
+    if (!query) {
+      setTeamChannels([]);
+      setDirectChannels([]);
     }
+  }, [query]);
 
-    const onSearch = (event) => { 
-        event.preventDefault()
-        setLoading(true)
-        setQuery(event.target.value)
-        getChannels(event.target.value)
+  const getChannels = async text => {
+    try {
+      const channelResponse = client.queryChannels({
+        type: "team",
+        name: { $autocomplete: text },
+        members: { $in: [client.userID] },
+      });
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID },
+        name: { $autocomplete: text },
+      });
+
+      const [channels, { users }] = await Promise.all([
+        channelResponse,
+        userResponse,
+      ]);
+
+      if (channels.length) setTeamChannels(channels);
+      if (users.length) setDirectChannels(users);
+    } catch (error) {
+      setQuery("");
     }
+  };
 
-    return (
-        <div className="channel-search__container">
-            <div className="channel-search__input__wrapper">
-                <div className="channel-search__input__icon">
-                    <SearchIcon />
-                </div>
-                <input
-                    className="channel-search__input__text"
-                    type="text" 
-                    placeholder="Search"
-                    value={query}
-                    onChange={onSearch}
-                    />
-            </div>
+  const onSearch = event => {
+    event.preventDefault();
+
+    setLoading(true);
+    setQuery(event.target.value);
+    getChannels(event.target.value);
+  };
+
+  const setChannel = channel => {
+    setQuery("");
+    setActiveChannel(channel);
+  };
+
+  return (
+    <div className="channel-search__container">
+      <div className="channel-search__input__wrapper">
+        <div className="channel-serach__input__icon">
+          <SearchIcon />
         </div>
-    )
-}
+        <input
+          className="channel-search__input__text"
+          placeholder="Search"
+          type="text"
+          value={query}
+          onChange={onSearch}
+        />
+      </div>
+      {query && (
+        <ResultsDropdown
+          teamChannels={teamChannels}
+          directChannels={directChannels}
+          loading={loading}
+          setChannel={setChannel}
+          setQuery={setQuery}
+          setToggleContainer={setToggleContainer}
+        />
+      )}
+    </div>
+  );
+};
 
-export default ChannelSearch
+export default ChannelSearch;
