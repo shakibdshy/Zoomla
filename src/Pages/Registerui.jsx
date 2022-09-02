2a85ea88749742e752ff29280d54a1ea75ed73ba
Md Habibur Rahman <shakibdshy@gmail.com>
2022-09-02 16:11:44 +0600

    Update Register UI

--- src/Pages/Registerui.jsx
+++ src/Pages/Registerui.jsx
@@ -16,14 +16,28 @@ import {
   FacebookButton,
   GoogleButton,
 } from "../components/SocialButtons/SocialButtons";
+import Cookies from "universal-cookie";
+import { StreamChat } from "stream-chat";
+import axios from "axios";
+import { Navigate, useLocation } from "react-router-dom";
+
+const cookies = new Cookies();
 
 export function ZoomlaAuth() {
   const [type, toggle] = useToggle(["login", "register"]);
+  const apiKey = "3pznn44zcu9w";
+  const client = StreamChat.getInstance(apiKey);
+  const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
   const form = useForm({
     initialValues: {
+      fullName: "",
+      username: "",
       email: "",
       name: "",
       password: "",
+      confirmPassword: "",
+      avatarURL: "",
       terms: true,
     },
 
@@ -35,6 +49,59 @@ export function ZoomlaAuth() {
           : null,
     },
   });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const { username, email, password, phoneNumber, avatarURL } = form;
+
+    console.log(form);
+
+    const URL = "https://zoomla-backend.herokuapp.com/auth";
+
+    const { data } = await axios.post(
+      `${URL}/${type ? "stream-signup" : "stream-login"}`,
+      {
+        username,
+        email,
+        password,
+        fullName: form.fullName,
+        phoneNumber,
+        avatarURL,
+      }
+    );
+    const { token, userId, hashedPassword, fullName } = data;
+    if (type === "register" && data) {
+      const userData = {
+        name: fullName,
+        email: username,
+        password: hashedPassword,
+        avatar: avatarURL,
+      };
+      const url = "https://zoomla-backend.herokuapp.com/api/auth";
+
+      await axios.post(`${url}/${type ? "signup" : "signin"}`, userData);
+      //console.log(data);
+    }
+
+    cookies.set("token", token);
+    cookies.set("username", username);
+    cookies.set("fullName", fullName);
+    cookies.set("email", email);
+    cookies.set("userId", userId);
+
+    if (type === "register") {
+      cookies.set("phoneNumber", phoneNumber);
+      cookies.set("avatarURL", avatarURL);
+      cookies.set("hashedPassword", hashedPassword);
+    }
+
+    window.location.reload();
+
+    if (client.user) {
+      Navigate(from, { replace: true });
+    }
+  };
 
   return (
     <Paper className="max-w-xl my-10 mx-auto" radius="md" p="xl" withBorder>
@@ -49,29 +116,52 @@ export function ZoomlaAuth() {
 
       <Divider label="Or continue with email" labelPosition="center" my="lg" />
 
-      <form onSubmit={form.onSubmit(() => {})}>
+      <form onSubmit={handleSubmit}>
         <Stack>
           {type === "register" && (
             <TextInput
               label="Name"
-              placeholder="Your name"
-              value={form.values.name}
+              placeholder="Your Full name"
+              value={form.values.fullName}
               onChange={event =>
-                form.setFieldValue("name", event.currentTarget.value)
+                form.setFieldValue("fullName", event.currentTarget.value)
               }
+            />
+          )}
+
+          {type === "register" && (
+            <TextInput
+              label="Email"
+              placeholder="Your email"
+              value={form.values.email}
+              onChange={event =>
+                form.setFieldValue("email", event.currentTarget.value)
+              }
+              error={form.errors.email && "Invalid email"}
             />
           )}
 
           <TextInput
             required
-            label="Email"
-            placeholder="hello@mantine.dev"
-            value={form.values.email}
+            label="Username"
+            placeholder="username"
+            value={form.values.username}
             onChange={event =>
-              form.setFieldValue("email", event.currentTarget.value)
+              form.setFieldValue("username", event.currentTarget.value)
             }
-            error={form.errors.email && "Invalid email"}
           />
+
+          {type === "register" && (
+            <TextInput
+              required
+              label="Avatar URL"
+              placeholder="Avatar URL"
+              value={form.values.avatarURL}
+              onChange={event =>
+                form.setFieldValue("avatarURL", event.currentTarget.value)
+              }
+            />
+          )}
 
           <PasswordInput
             required
@@ -86,6 +176,22 @@ export function ZoomlaAuth() {
               "Password should include at least 6 characters"
             }
           />
+
+          {type === "register" && (
+            <PasswordInput
+              required
+              label="Confirm Password"
+              placeholder="Confirm password"
+              value={form.values.password}
+              onChange={event =>
+                form.setFieldValue("password", event.currentTarget.value)
+              }
+              error={
+                form.errors.password &&
+                "Password should include at least 6 characters"
+              }
+            />
+          )}
 
           {type === "register" && (
             <Checkbox
