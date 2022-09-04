10d4a53fb2d47eabe3b4f268fa497ce6e08fab97
Md Habibur Rahman <shakibdshy@gmail.com>
2022-09-04 15:41:32 +0600

    Update Card Section

--- src/Shared/Card.jsx
+++ src/Shared/Card.jsx
@@ -13,88 +13,95 @@ const cookies = new Cookies();
 function Card() {
     const [open, setOpen] = useState(false);
     const [meetOpen, setMeetOpen] = useState(false);
-    const handleOpen = (index) => {
-        if (index === 2) {
-            setOpen(!open);
-        }
-        else if (index === 1) {
-            setMeetOpen(!open)
-        } else if (index === 0) {
-            fetch("https://prod-in2.100ms.live/api/v2/rooms", {
-                method: "POST",
-                headers: {
-                    'Authorization': "Bearer " + cookies.get("hmsToken"),
-                    "Content-Type": "application/json"
-                },
-                body: {
-                    "name": "test-room",
-                    "description": "This is a test room",
-                    "recording_info": {
-                        "enabled": true,
-                        "upload_info": {
-                            "type": "s3",
-                            "location": "test-bucket",
-                            "prefix": "test-prefix",
-                            "options": { "region": "ap-south-1" },
-                            "credentials": { "key": "aws-access-key", "secret": "aws-secret-key" }
+
+    const createMeeting = () => {
+        fetch('https://prod-in2.100ms.live/api/v2/rooms', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + cookies.get('token'),
+                'Content-Type': 'application/json'
+            },
+            // body: '{"name": "test-room", "description": "This is a test room", "recording_info": {"enabled": true, "upload_info": {"type": "s3", "location": "test-bucket", "prefix": "test-prefix", "options": {"region": "ap-south-1"}, "credentials": {"key": "aws-access-key", "secret": "aws-secret-key"}}}}',
+            body: JSON.stringify({
+                'name': 'meeting-room',
+                'description': 'This is a test room',
+                'recording_info': {
+                    'enabled': true,
+                    'upload_info': {
+                        'type': 's3',
+                        'location': 'test-bucket',
+                        'prefix': 'test-prefix',
+                        'options': {
+                            'region': 'ap-south-1'
+                        },
+                        'credentials': {
+                            'key': 'aws-access-key',
+                            'secret': 'aws-secret-key'
                         }
                     }
                 }
-            }).then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                })
-            /**
-             * 'Authorization: Bearer  <management_token>'
-             *  -X POST -H 
-             * 'Content-Type: application/json'
-             *  -d '
+            })
+        }).then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+}
 
-             */
-        }
+const handleOpen = (index) => {
+    if (index === 2) {
+        setOpen(!open);
     }
+    else if (index === 1) {
+        setMeetOpen(!open)
+    }
+}
 
-    const cards = [
+const cards = [
+    {
+        link: '',
+        icon: <FaVideo />,
+        title: 'New Meeting',
+        short: 'set up new meeting',
+    },
+    {
+        link: '',
+        icon: <BsPlusSquareFill />,
+        title: 'Join Meeting',
+        short: 'Via invitation link'
+    },
+    {
+        link: '',
+        icon: <BsFillCalendarDateFill />,
+        title: 'Schedule',
+        short: 'plan your meeting'
+    },
+    {
+        link: '',
+        icon: <TbScreenShare />,
+        title: 'Share Screen',
+        short: 'show your work'
+    },
+]
+return (
+    <div className='grid justify-center grid-cols-2 md:gap-8 gap-5 max-w-xl mx-auto'>
+        <Link to="/" onClick={() => createMeeting()} className='card cursor-pointer'>
+            <div className='card-icon'><TbScreenShare /></div>
+            <h4 className='card-title'>New Meeting</h4>
+            <p className='card-text'>Via invitation link</p>
+        </Link>
         {
-            link: '',
-            icon: <FaVideo />,
-            title: 'New Meeting',
-            short: 'set up new meeting',
-        },
-        {
-            link: '',
-            icon: <BsPlusSquareFill />,
-            title: 'Join Meeting',
-            short: 'Via invitation link'
-        },
-        {
-            link: '',
-            icon: <BsFillCalendarDateFill />,
-            title: 'Schedule',
-            short: 'plan your meeting'
-        },
-        {
-            link: '',
-            icon: <TbScreenShare />,
-            title: 'Share Screen',
-            short: 'show your work'
-        },
-    ]
-    return (
-        <div className='grid justify-center grid-cols-2 md:gap-8 gap-5 max-w-xl mx-auto'>
-            {
-                cards.map((card, index) => (
-                    <Link to={card.link} key={index} onClick={() => handleOpen(index)} className='card cursor-pointer'>
-                        <div className='card-icon'>{card.icon}</div>
-                        <h4 className='card-title'>{card.title}</h4>
-                        <p className='card-text'>{card.short}</p>
-                    </Link>
-                ))
-            }
-            <ScheduleModul setOpen={setOpen} open={open} />
-            <JoinMeetingModal setMeetOpen={setMeetOpen} open={meetOpen} />
-        </div>
-    )
+            cards.map((card, index) => (
+                <Link to={card.link} key={index} onClick={() => handleOpen(index)} className='card cursor-pointer'>
+                    <div className='card-icon'>{card.icon}</div>
+                    <h4 className='card-title'>{card.title}</h4>
+                    <p className='card-text'>{card.short}</p>
+                </Link>
+            ))
+        }
+        <ScheduleModul setOpen={setOpen} open={open} />
+        <JoinMeetingModal setMeetOpen={setMeetOpen} open={meetOpen} />
+    </div>
+)
 }
 
 export default Card
